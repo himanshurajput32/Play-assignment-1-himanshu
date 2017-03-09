b93d6c781bab7299a08ad02bdd1ee562a911d6fa
himanshu <himanshu.rajput@gmail.com>
2017-03-09 18:31:03 +0530

    testing done

--- test/ControllerSpec.scala
+++ test/ControllerSpec.scala
@@ -1,25 +1,30 @@
 
+import controllers.{HomeController, LoginController, ProfileController, SignupController}
+import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play._
+import play.api.cache.CacheApi
 import play.api.test._
 import play.api.test.Helpers.{contentAsString, _}
+import services.MyService
 
 /**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-class ControllerSpec extends PlaySpec with OneAppPerTest {
-
+  * Add your spec here.
+  * You can mock out a whole application including requests, plugins etc.
+  * For more information, consult the wiki.
+  */
+class ControllerSpec extends PlaySpec with OneAppPerTest with MockitoSugar {
 
 
   "HomeController" should {
 
     "render the index page" in {
-      val home = route(app, FakeRequest(GET, "/")).get
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play App")
+      val home = new HomeController
+      val result = home.index().apply(FakeRequest())
+
+      status(result) mustBe OK
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Welcome to Play App")
     }
 
   }
@@ -27,24 +32,38 @@ class ControllerSpec extends PlaySpec with OneAppPerTest {
   "SignupController" should {
 
     "render signup page" in {
-      status(route(app, FakeRequest(GET, "/signup")).get) mustBe OK
-      contentAsString(route(app, FakeRequest(GET, "/signup")).get) must include("Create an Account")
+      val service = mock[MyService]
+      val home = new SignupController(service)
+      val result = home.signup().apply(FakeRequest())
+      status(result) mustBe OK
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Create an Account")
+
     }
 
   }
   "LoginController" should {
 
     "render login page" in {
-      status(route(app, FakeRequest(GET, "/login")).get) mustBe OK
-      contentAsString(route(app, FakeRequest(GET, "/login")).get) must include("Login")
+      val service = mock[MyService]
+      val home = new LoginController(service)
+      val result = home.login().apply(FakeRequest())
+      status(result) mustBe OK
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Login")
     }
 
   }
   "ProfileController" should {
 
     "render logout page" in {
-      status(route(app, FakeRequest(GET, "/logout")).get) mustBe OK
-      contentAsString(route(app, FakeRequest(GET, "/login")).get) must include("Login")
+      val service = mock[MyService]
+      val cache=mock[CacheApi]
+      val home = new ProfileController(cache,service)
+      val result = home.logout().apply(FakeRequest())
+      status(result) mustBe OK
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Login")
     }
 
   }
